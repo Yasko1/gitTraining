885be446a63b706aab91b87877f769e9aaa4c79b
Hanna <32440084+Yasko1@users.noreply.github.com>
2019-03-19 20:28:45 +0300

    Add files via upload

---
+++ PracticeTask02/src/by/epam/javatrining/main/Main.java
@@ -0,0 +1,19 @@
+package by.epam.javatrining.main;
+
+import by.epam.javatrining.*;
+
+public class Main {
+	
+	public static void main (String[] args){
+		
+        ReasearchOverOperation.testInt();
+        ReasearchOverOperation.testShort();
+        ReasearchOverOperation.testByte();
+        ReasearchOverOperation.testChar();
+        ReasearchOverOperation.testLong();
+        ReasearchOverOperation.testFloat();
+        ReasearchOverOperation.testDouble();
+        ReasearchOverOperation.testBoolean();
+        
+    }
+}
